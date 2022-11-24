294a9728a04800aeecd59b5e16e9542f7edf9d03
Dino Capek <dino.capek15@gmail.com>
2022-11-24 19:07:25 +0100

    Created initial UI with menu and panel.

---
+++ DigitalSignature/DigitalSignature/DSMain.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace DigitalSignature
+{
+    partial class DSMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DSMain));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.StSim = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.StAsim = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.KDSim = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.KDAsim = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.IzrSaz = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel6 = new System.Windows.Forms.ToolStripLabel();
+            this.panel = new System.Windows.Forms.Panel();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StSim,
+            this.toolStripSeparator1,
+            this.StAsim,
+            this.toolStripSeparator2,
+            this.KDSim,
+            this.toolStripSeparator3,
+            this.KDAsim,
+            this.toolStripSeparator4,
+            this.IzrSaz,
+            this.toolStripSeparator5,
+            this.toolStripLabel6});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(971, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // StSim
+            // 
+            this.StSim.Name = "StSim";
+            this.StSim.Size = new System.Drawing.Size(144, 22);
+            this.StSim.Text = "Stvaranje ključa sim. kript.";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.toolStripSeparator1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // StAsim
+            // 
+            this.StAsim.Name = "StAsim";
+            this.StAsim.Size = new System.Drawing.Size(162, 22);
+            this.StAsim.Text = "Stvaranje ključeva asim. kript.";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // KDSim
+            // 
+            this.KDSim.Name = "KDSim";
+            this.KDSim.Size = new System.Drawing.Size(179, 22);
+            this.KDSim.Text = "Kriptiranje/dekriptiranje sim. alg.";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            // 
+            // KDAsim
+            // 
+            this.KDAsim.Name = "KDAsim";
+            this.KDAsim.Size = new System.Drawing.Size(185, 22);
+            this.KDAsim.Text = "Kriptiranje/dekriptiranje asim. alg.";
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 25);
+            // 
+            // IzrSaz
+            // 
+            this.IzrSaz.Name = "IzrSaz";
+            this.IzrSaz.Size = new System.Drawing.Size(120, 22);
+            this.IzrSaz.Text = "Izračunavanje sažetka";
+            // 
+            // toolStripSeparator5
+            // 
+            this.toolStripSeparator5.Name = "toolStripSeparator5";
+            this.toolStripSeparator5.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel6
+            // 
+            this.toolStripLabel6.Name = "toolStripLabel6";
+            this.toolStripLabel6.Size = new System.Drawing.Size(122, 15);
+            this.toolStripLabel6.Text = "Digitalno potpisivanje";
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(0, 28);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(971, 528);
+            this.panel.TabIndex = 1;
+            // 
+            // DSMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(971, 558);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "DSMain";
+            this.Text = "Digitalni potpis";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel StSim;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel StAsim;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripLabel KDSim;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripLabel KDAsim;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripLabel IzrSaz;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel6;
+        private System.Windows.Forms.Panel panel;
+    }
+}
+
--- DigitalSignature/DigitalSignature/DSMain.cs
+++ DigitalSignature/DigitalSignature/DSMain.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace DigitalSignature
 {
-    public partial class Form1 : Form
+    public partial class DSMain : Form
     {
-        public Form1()
+        public DSMain()
         {
             InitializeComponent();
         }
